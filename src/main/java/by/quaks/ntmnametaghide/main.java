ae8e69a84c1c3886333eb24e7df116e302841426
Quaks <egorlapatskij@gmail.com>
2022-04-24 20:54:20 +0300

    Initial commit

---
+++ src/main/java/by/quaks/ntmnametaghide/main.java
@@ -0,0 +1,36 @@
+package by.quaks.ntmnametaghide;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scoreboard.NameTagVisibility;
+import org.bukkit.scoreboard.Scoreboard;
+import org.bukkit.scoreboard.Team;
+
+public final class main extends JavaPlugin implements Listener {
+    public static Scoreboard scoreboard;
+    public static Team team;
+
+    @Override
+    public void onEnable() {
+        scoreboard = Bukkit.getScoreboardManager().getNewScoreboard();
+        team = scoreboard.registerNewTeam("hide_nametag");
+        team.setNameTagVisibility(NameTagVisibility.NEVER);
+        team.setCanSeeFriendlyInvisibles(false);
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent e) {
+        apply(e.getPlayer());
+    }
+
+    public void apply(Player p) {
+        if (team.getPlayers().contains(p)) {
+            team.addPlayer(p);
+            p.setScoreboard(scoreboard);
+        }
+    }
+}
